1584f7790585909ea9c0c8e4818260cf871839f9
edith.githinji <edithgithinji2020@gmail.com>
2025-01-29 23:07:11 +0300

    a dynamic new post form

--- src/Components/NewPostForm.js
+++ src/Components/NewPostForm.js
@@ -1,67 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import '../App.css'
+import { useNavigate } from 'react-router-dom'
 
-
-function NewPostForm({ addPost, setPosts }) {
-    const [newPost, setNewPost] = useState({ title: '', content: '', author: '', image: '' });
+const NewPostForm = ({ addPost, setBlogs }) => {
+    const navigate = useNavigate();
+    const [newPost, setNewPost] = useState({
+        title: '',
+        content: '',
+        author: '',
+        image: '',
+        comments: [],
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-
         fetch('http://localhost:3001/blogs', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'content-Type': "application/json" },
             body: JSON.stringify(newPost),
-        }
-        )
-            .then((res) => res.json())
+        })
+            .then((response) => response.json())
             .then((newPost) => {
-                setNewPost({ title: '', content: '', author: '', image: '' });
+                setNewPost({ title: '', content: '', author: '', image: '' })
                 addPost(newPost)
                 window.location.href = '/blogs';
-
-            });
-    };
-    
-
+            })
+    }
     return (
         <div className='add-post'>
             <h2>Add Post</h2>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={newPost.title}
-                    onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-                    required
-                />
-                <textarea
-                    placeholder="Content"
-                    value={newPost.content}
-                    onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
-                    required
-                />
-                <input
-                    type="text"
-                    placeholder="Author"
-                    value={newPost.author}
-                    onChange={(e) => setNewPost({ ...newPost, author: e.target.value })}
-                    required
-                />
-                <input
-                    type="text"
-                    placeholder="Image URL"
-                    value={newPost.image}
-                    onChange={(e) => setNewPost({ ...newPost, image: e.target.value })}
-                    required
-                    pattern="https?://.*" // This regex ensures the input is a valid URL starting with http:// or https://
-                    title="Please enter a valid URL starting with http:// or https://"
-                />
-                <button type="submit">Add Post</button>
+                <label>
+                    Title:
+                    <input
+                        type='text'
+                        value={newPost.title}
+                        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+                        required
+                    />
+                </label>
+                <label>
+                    Content:
+                    <textarea
+                        value={newPost.Content}
+                        onChange={(e) =>
+                            setNewPost({ ...newPost, content: e.target.value })
+                        }
+                        required
+                    />
+                </label>
+                <label>
+                    Author:
+                    <input
+                        type='text'
+                        value={newPost.author}
+                        onChange={(e) => setNewPost({ ...newPost, author: e.target.value })}
+                        required
+                    />
+                </label>
+                <label>
+                    Image URL(optional):
+                    <input
+                        type='text'
+                        placeholder='Image Url'
+                        value={newPost.image}
+                        onChange={(e) => setNewPost({ ...newPost, image: e.target.value })}
+                        pattern='https?://.*'
+                        title='Please enter a valid image URL starting with http:// or https://'
+                    />
+                </label>
+                <button type='submit'>Create Post</button>
             </form>
+
         </div>
-    );
+    )
 }
 
-export default NewPostForm;
+export default NewPostForm
