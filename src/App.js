e9f30abc8991e1f76a0ad77d88853b762eb37907
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-01-29 12:38:29 +0300

    remove blog state from bloglist.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import BlogList from './Components/BlogList.js';
 
 function App() {
+     const [blogs, setBlogs] = useState([])
+      console.log(blogs)
+       //fetching the data from an API
+       useEffect(()=> {fetch("http://localhost:3001/blogs")
+       .then((res)=> res.json())
+       .then((data)=> setBlogs(data))
+       .catch((error) => console.error('Error fetching blogs:', error));
+   
+   }, [])
   return (
      <div>
-      <h1>Blog</h1>
-      <BlogList/>
+      <BlogList blogs={blogs} setBlogs={setBlogs}/>
      </div>
   );
 }
--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,17 +1,7 @@
 import {useState,useEffect} from 'react'
 import BlogPost from './BlogPost'
 
-function BlogList(){
-    //initializing the state
-    const [blogs, setBlogs] = useState([])
-   console.log(blogs)
-    //fetching the data from an API
-    useEffect(()=> {fetch("http://localhost:3001/blogs")
-    .then((res)=> res.json())
-    .then((data)=> setBlogs(data))
-    .catch((error) => console.error('Error fetching blogs:', error));
-
-}, [])
+function BlogList({blogs,setBlogs}){
   return(
     <div>
          {blogs.length === 0 ? (
