8ee37d0ee7d3620b401c342beca39c833554a3b0
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-01-29 21:37:26 +0300

    define state and fetch data in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import BlogList from './Components/BlogList.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import BlogList from './Components/BlogList';
+import BlogPost from './Components/BlogPost';
+import NewPostForm from './Components/NewPostForm';
+import Error from './error';
+import { useEffect,useState } from'react';
+
 
 function App() {
-     const [blogs, setBlogs] = useState([])
-      console.log(blogs)
-       //fetching the data from an API
-       useEffect(()=> {fetch("http://localhost:3001/blogs")
-       .then((res)=> res.json())
-       .then((data)=> setBlogs(data))
-       .catch((error) => console.error('Error fetching blogs:', error));
-   
-   }, [])
+     const [blogs, setBlogs] = useState([]);
+    
+      useEffect(() => {
+        fetch('http://localhost:3001/blogs')
+          .then((res) => res.json())
+          .then((data) => setBlogs(data));
+      }, []);
+
+     // Handle new post addition
+     const handleNewBlogs = (newPosts) => {
+        setBlogs(prevBlogs => [newPosts, ...prevBlogs]); // Functional update
+      };
+
+
   return (
-     <div>
-      <BlogList blogs={blogs} setBlogs={setBlogs}/>
-     </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/blogs" element={<BlogList blogs={blogs} setBlogs={setBlogs} />} />
+        <Route path="/blogs/:id" element={<BlogPost />} />
+        <Route path="/new-post" element={<NewPostForm addPost={handleNewBlogs} setBlogs={setBlogs}/>} />
+        <Route path="*" element={<Error/>} />
+      </Routes>
+    </Router>
   );
 }
 
