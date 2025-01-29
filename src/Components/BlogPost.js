cd79692167cf309730dbc5d87762204720a16a76
kin <brianmkcnight@gmail.com>
2025-01-29 10:21:27 +0300

    updated src/Components/BlogPost.js

--- src/Components/BlogPost.js
+++ src/Components/BlogPost.js
@@ -1,40 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import CommentSection from './CommentSection';
 
-
 function BlogPost() {
-  const { id } = ();
+  const { id } = useParams();
   const [blog, setBlog] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:3001/blogs/${id}`)
-       .then((res) => res.json())
-       .then((data) => setBlog(data));
+      .then((res) => res.json())
+      .then((data) => setBlog(data));
   }, [id]);
 
   const handleDelete = () => {
-    fetch(`http://localhost:3001/blogs/${id}`, { method: 'DELETE'} )
-    .then(() => {
-       window.location.href = './blogs';
-    });
+    fetch(`http://localhost:3001/blogs/${id}`, { method: 'DELETE' })
+      .then(() => {
+        window.location.href = '/blogs'; // Redirect to blog list after deletion
+      });
   };
 
-  if (!blog) return <div>Loading....</div>;
+  if (!blog) return <div>Loading...</div>;
 
-  
   return (
     <div>
-        <h1>{blog.title}</h1>
-        <h3>{blog.auther}</h3>
-        <img src={blog.image} alt={blog.title} />
-        <p>By: {blog.content}</p>
-        <CommentSection blogId={blog.id} comment= {blog.comment} />
-        <button onClick={handleDelete}>Delete Post</button>
+      <h1>{blog.title}</h1>
+      <h3>By {blog.author}</h3>
+      <img src={blog.image} alt={blog.title} />
+      <p>{blog.content}</p>
+      <CommentSection blogId={blog.id} comments={blog.comments} />
+      <button onClick={handleDelete}>Delete Post</button>
     </div>
   );
 }
 
 export default BlogPost;
-
-
-
